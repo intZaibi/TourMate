c211dde60c1f049a19ed991e2e4e1affc1971e1a
IntZaibi <154490572+intZaibi@users.noreply.github.com>
2025-05-09 12:16:11 +0500

    Update Trip.ts

--- models/Trip.ts
+++ models/Trip.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-mongoose.connect('mongodb://127.0.0.1:27017/test');
+mongoose.connect(process.env.DB_URL || 'mongodb://127.0.0.1:27017/test');
 
 const TripSchema = new mongoose.Schema({
   recommendation: [{
